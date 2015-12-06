5cf3eb8b5e47f64dc88430346fdf34aa10a2a76d
HungryAnt <ljsunlin@126.com>
2015-12-06 23:04:16 +0800

    up port

--- server/src/chat_room_server.rb
+++ server/src/chat_room_server.rb
@@ -94,7 +94,7 @@ class ChatRoomServer
   def init
     # server = TCPServer.open(2003)
     # loop {
-    Socket.tcp_server_loop(2007) do |client, client_addrinfo|
+    Socket.tcp_server_loop(2008) do |client, client_addrinfo|
       # Thread.start(server.accept) do |client|
       Thread.new {
         begin
